a5abfb3d6140d70de5aadc4715af3cce3ee16b42
nworbnitsuj <justin.v.brown@gmail.com>
2018-03-25 14:38:38 -0500

    added styling

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("It's alive!");
+
+const movePlayer1 = () => {
+  document.getElementById('player1').removeAttribute('id');
+  document.getElementsByTagName('td')[1].setAttribute('id', 'player1');
+}
+
+movePlayer1();
